59fa62dad58066c43f762ee658955129736c8866
Justin <justinherrmann34@gmail.com>
2023-06-21 13:39:33 +0100

    Drittes Rätsel fertig

--- Assets/+++Workdata/Script/PlayerController.cs
+++ Assets/+++Workdata/Script/PlayerController.cs
@@ -57,6 +57,43 @@ public class PlayerController : MonoBehaviour
 
     public GameObject empty_Bookshelf;
 
+    public GameObject proteinpowder_Outside;
+
+    public GameObject handcuffs_Outside;
+
+    public GameObject ribbons_Outside;
+
+    public GameObject proteinpowder_Inventar;
+
+    public GameObject handcuffs_Inventar;
+
+    public GameObject ribbon_Inventar;
+
+    public GameObject protein_Statue;
+
+    public GameObject protein_Empty_Statue;
+
+    public GameObject handcuff_Statue_Fake_1;
+
+    public GameObject ribbon_Statue_Fake_1;
+
+    public GameObject handcuff_Statue;
+
+    public GameObject handcuff_Empty_Statue;
+
+    public GameObject protein_Statue_Fake_1;
+
+    public GameObject ribbon_Statue_Fake_2;
+
+    public GameObject ribbon_Statue;
+
+    public GameObject ribbon_empty_Statue;
+
+    public GameObject protein_Statue_Fake_2;
+
+    public GameObject handcuff_Statue_Fake_2;
+ 
+
     #endregion 
 
     #region Serialized field
@@ -137,26 +174,23 @@ public class PlayerController : MonoBehaviour
             interact_button_text_castle_room.SetActive(true);
             interact_count = 1;
         }
-
-        if (collision.CompareTag("Lever_2"))
+        else if (collision.CompareTag("Lever_2"))
         {
             interact_button_text_castle_room.SetActive(true);
             interact_count = 2;
         }
-
-        if (collision.CompareTag("Lever_3"))
+        else if (collision.CompareTag("Lever_3"))
         {
             interact_button_text_castle_room.SetActive(true);
             interact_count = 3;
         }
-
-        if (collision.CompareTag("Lever_4"))
+        else if (collision.CompareTag("Lever_4"))
         {
             interact_button_text_castle_room.SetActive(true);
             interact_count = 4;
         }
 
-        if (collision.CompareTag("Book"))
+       if (collision.CompareTag("Book"))
         {
             book_count = 1;
         }
@@ -174,7 +208,32 @@ public class PlayerController : MonoBehaviour
         {
             interact_count = 6;
         }
+        else if (collision.CompareTag("Proteinpowder"))
+        {
+            interact_count = 7;
+        }
+        else if (collision.CompareTag("Handcuffs"))
+        {
+            interact_count = 8;
+        }
+        else if (collision.CompareTag("Ribbon"))
+        {
+            interact_count = 9;
+        }
+        else if (collision.CompareTag("Proteinpowder_Statue"))
+        {
+            interact_count = 10;
+        }
+        else if (collision.CompareTag("Handcuff_Statue"))
+        {
+            interact_count = 11;
+        }
+        else if (collision.CompareTag("Ribbon_Statue"))
+        {
+            interact_count = 12;
+        }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Lever_1"))
@@ -198,9 +257,43 @@ public class PlayerController : MonoBehaviour
             interact_count = 0;
         }
 
+        if (collision.CompareTag("Door_1"))
+        {
+            interact_count = 0;
+        }
+        else if (collision.CompareTag("Door_2"))
+        {
+            interact_count = 0;
+        }
+
         if (collision.CompareTag("Book"))
         {
             book_count = 0;
+        }
+
+        if (collision.CompareTag("Proteinpowder"))
+        {
+            interact_count = 0;
+        }
+        else if (collision.CompareTag("Handcuffs"))
+        {
+            interact_count = 0;
+        }
+        else if (collision.CompareTag("Ribbon"))
+        {
+            interact_count = 0;
+        }
+        else if (collision.CompareTag("Proteinpowder_Statue"))
+        {
+            interact_count = 0;
+        }
+        else if (collision.CompareTag("Handcuff_Statue"))
+        {
+            interact_count = 0;
+        }
+        else if (collision.CompareTag("Ribbon_Statue"))
+        {
+            interact_count = 0;
         }
     }
     #endregion
@@ -261,6 +354,168 @@ public class PlayerController : MonoBehaviour
          if(context.performed && book_count == 2)
         {
             StartCoroutine("Wait");
+        }
+
+         if(context.performed && interact_count == 7 && inventar_count == 0)
+        {
+            proteinpowder_Outside.SetActive(false);
+            proteinpowder_Inventar.SetActive(true);
+            inventar_count = 1;
+        }
+         else if(context.performed && interact_count == 8 && inventar_count == 0)
+        {
+            handcuffs_Outside.SetActive(false);
+            handcuffs_Inventar.SetActive(true);
+            inventar_count = 1;
+        }
+         else if(context.performed && interact_count == 9 && inventar_count == 0)
+        {
+            ribbons_Outside.SetActive(false);
+            ribbon_Inventar.SetActive(true);
+            inventar_count = 1;
+        }
+
+         if(proteinpowder_Inventar.activeInHierarchy && context.performed && interact_count == 10)
+        {
+            protein_Empty_Statue.SetActive(false);
+            protein_Statue.SetActive(true);
+
+            proteinpowder_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+         else if(handcuffs_Inventar.activeInHierarchy && context.performed && interact_count == 10)
+        {
+            protein_Empty_Statue.SetActive(false);
+            handcuff_Statue_Fake_1.SetActive(true);
+
+            handcuffs_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+        else if (ribbon_Inventar.activeInHierarchy && context.performed && interact_count == 10)
+        {
+            protein_Empty_Statue.SetActive(false);
+            ribbon_Statue_Fake_1.SetActive(true);
+
+            ribbon_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+
+        if (handcuffs_Inventar.activeInHierarchy && context.performed && interact_count == 11)
+        {
+            handcuff_Empty_Statue.SetActive(false);
+            handcuff_Statue.SetActive(true);
+
+            handcuffs_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+        else if(proteinpowder_Inventar.activeInHierarchy && context.performed && interact_count == 11)
+        {
+
+            handcuff_Empty_Statue.SetActive(false);
+            protein_Statue_Fake_1.SetActive(true);
+
+            proteinpowder_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+        else if(ribbon_Inventar.activeInHierarchy && context.performed && interact_count == 11)
+        {
+
+            handcuff_Empty_Statue.SetActive(false);
+            ribbon_Statue_Fake_2.SetActive(true);
+
+            ribbon_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+
+        if(ribbon_Inventar.activeInHierarchy && context.performed && interact_count == 12)
+        {
+            ribbon_empty_Statue.SetActive(false);
+            ribbon_Statue.SetActive(true);
+
+            ribbon_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+        else if(proteinpowder_Inventar.activeInHierarchy && context.performed && interact_count == 12)
+        {
+            ribbon_empty_Statue.SetActive(false);
+            protein_Statue_Fake_2.SetActive(true);
+
+            proteinpowder_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+        else if (handcuffs_Inventar.activeInHierarchy && context.performed && interact_count == 12)
+        {
+            ribbon_empty_Statue.SetActive(false);
+            handcuff_Statue_Fake_2.SetActive(true);
+
+            handcuffs_Inventar.SetActive(false);
+
+            inventar_count = 0;
+        }
+
+        if(context.performed && interact_count == 10 && inventar_count == 0 && handcuff_Statue_Fake_1.activeInHierarchy)
+        {
+            protein_Empty_Statue.SetActive(true);
+            handcuff_Statue_Fake_1.SetActive(false);
+
+            handcuffs_Inventar.SetActive(true);
+
+            interact_count = 1;
+        }
+        else if(context.performed && interact_count == 10 && inventar_count == 0 && ribbon_Statue_Fake_1.activeInHierarchy)
+        {
+            protein_Empty_Statue.SetActive(true);
+            ribbon_Statue_Fake_1.SetActive(false);
+
+            ribbon_Inventar.SetActive(true);
+
+            interact_count = 1;
+        }
+
+        if(context.performed && interact_count == 11 && inventar_count == 0 && protein_Statue_Fake_1.activeInHierarchy)
+        {
+            handcuff_Empty_Statue.SetActive(true);
+            protein_Statue_Fake_1.SetActive(false);
+
+            proteinpowder_Inventar.SetActive(true);
+
+            interact_count = 1; 
+        }
+        else if (context.performed && interact_count == 11 && inventar_count == 0 && ribbon_Statue_Fake_2.activeInHierarchy)
+        {
+            handcuff_Empty_Statue.SetActive(true);
+            ribbon_Statue_Fake_2.SetActive(false);
+
+            ribbon_Inventar.SetActive(true);
+
+            interact_count = 1;
+        }
+
+        if(context.performed && interact_count == 12 && inventar_count == 0 && protein_Statue_Fake_2.activeInHierarchy)
+        {
+            ribbon_empty_Statue.SetActive(true);
+            protein_Statue_Fake_2.SetActive(false);
+
+            proteinpowder_Inventar.SetActive(true);
+
+            inventar_count = 1;
+        }
+        else if (context.performed && interact_count == 12 && inventar_count == 0 && handcuff_Statue_Fake_2.activeInHierarchy)
+        {
+            ribbon_empty_Statue.SetActive(true);
+            handcuff_Statue_Fake_2.SetActive(false);
+
+            handcuffs_Inventar.SetActive(true);
+
+            inventar_count = 1;
         }
     }
 
