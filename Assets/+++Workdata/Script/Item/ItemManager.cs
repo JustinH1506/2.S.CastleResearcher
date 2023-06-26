b89565f4a20676350e02c3d6be86ffe32e6218fc
Justin <justinherrmann34@gmail.com>
2023-06-26 13:13:39 +0100

    SafeState

--- Assets/+++Workdata/Script/Item/ItemManager.cs
+++ Assets/+++Workdata/Script/Item/ItemManager.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    [SerializeField] ItemBehaviour[] itemBehaviour;
-    [SerializeField] GameObject ball;
-    [SerializeField] GameObject handcuff;
-    [SerializeField] GameObject ribbon;
+    [SerializeField] ItemBehaviour itemBehaviour;
+    [SerializeField] GameObject[] itemObjects_InGame;
+    [SerializeField] GameObject[] itemObjects_Inventar;
 
+    public bool CheckItems(int itemID)
+    {
+      for (int i = 0; i <= itemID; i++)
+        {
+            if (itemBehaviour.pressed)
+            {
+                itemObjects_InGame[i].SetActive(false);
 
-    
+                itemObjects_Inventar[i].SetActive(true);
+
+                return true;
+            }
+        }
+      return false;
+    }
 }
